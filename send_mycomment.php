e44e99c16bc7f645565a602b845b7bf6ad6a8eb0
zeeshan <zee2790@gmail.com>
2015-11-14 22:40:37 +0400

    update get

---
+++ send_mycomment.php
@@ -0,0 +1,46 @@
+<?php
+
+$servername = "54.254.209.190";
+$username = "root";
+$password = "hassan";
+$dbname = "a3214356_yikyak";
+
+$conn = new mysqli($servername, $username, $password, $dbname);
+// Check connection
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+ 
+//$ok=0;
+
+$comemail = $_POST["comemail"];
+$comuuid = $_POST["comuuid"];
+$postid = $_POST["postid"];
+//$userid = $_POST["userid"];
+//$emailid = $_POST["emailid"];
+$postid=(int)$postid;
+$commenttext = $_POST["commenttext"];
+
+// $postid = "444";
+// $userid = "357503050188210";
+// $emailid = "aaaa";
+// $postid=(int)$postid;
+// $commenttext = "dddddddddd";
+// $comuuid='357503050188210';
+// $comemail='222';
+$ok=0;
+$sql = "INSERT INTO comment (postid,commenttext,userid,emailid)
+VALUES ('$postid','$commenttext','$comuuid',$comemail)";
+
+
+if ($conn->query($sql) === TRUE) {
+    echo "successfully";
+$ok=1;
+} else {
+    echo "nosuccessfully";
+}
+
+
+$conn->close();
+exit();
+?>
