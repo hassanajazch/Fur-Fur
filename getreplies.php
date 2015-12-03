b1adc15a856e85fa93c148e7ebbe3aecf158a4e7
zeeshan <zee2790@gmail.com>
2015-12-03 12:33:24 +0400

    jjj

---
+++ getreplies.php
@@ -0,0 +1,32 @@
+<?php
+
+$servername = "54.254.209.190";
+$username = "root";
+$password = "hassan";
+
+$dbname = "a3214356_yikyak";
+$conn = new mysqli($servername, $username, $password, $dbname);
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+} 
+$postid=urldecode($_POST["postid"]);
+//$posttext=urldecode($_POST["posttext"]);
+  // $postid=(int)$postid;
+
+	//$sql = "select * from newpost " ;
+$sql="SELECT * FROM commenttable where userid='$postid'";
+	$result = $conn->query($sql);
+	$userData = array();
+	if ($result->num_rows > 0) {
+	    // output data of each row
+	    while($row = $result->fetch_assoc()) {
+			$tmp = array('postid' => $row["postid"],  'commenttext' => $row["commenttext"],    'imgurl' => $row["imgurl"]);
+			array_push($userData, $tmp);
+			
+		}
+	}
+//$userData = array_reverse($userData,true);
+	echo json_encode($userData);
+	$conn->close();
+	exit();
+?>	
