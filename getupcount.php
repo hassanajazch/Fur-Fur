763adf6a7efe23b892478e6ce71d73c6e858966d
zeeshan <zee2790@gmail.com>
2015-12-05 14:26:44 +0400

    hello

--- getupcount.php
+++ getupcount.php
@@ -4,36 +4,27 @@ $servername = "54.254.209.190";
 $username = "root";
 $password = "hassan";
 $dbname = "a3214356_yikyak";
-
 $conn = new mysqli($servername, $username, $password, $dbname);
 if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 } 
 $id=urldecode($_POST["id"]);
-
 $uuid=urldecode($_POST["uuid"]);
 $email=urldecode($_POST["email"]);
 $regno=urldecode($_POST["regno"]);
 $ok=0;
-//$id='102';
+//$id='53';
 //$uuid='357503050188210';
-//$email='12019020027@umt.edu.pk';
-//$regno="1";
-//$posttext=urldecode($_POST["posttext"]);
-   //$id=(int)$id;
-//ll
+$sql="UPDATE mynewpost SET count=count+1 WHERE id='$id'";
 	
-$sql="UPDATE mynewpost SET count=count+1 WHERE id='$id' ";
-//$sql="SELECT * FROM comment postid='$postid'";
-	//$result = $conn->query($sql);
+//$result = $conn->query($sql);
+
 if ($conn->query($sql) === TRUE) {
     echo "sucessfully";
 $ok=1;
 } else {
-
-  echo "nosucessfully";
+    echo "nosucessfully";
 }
-
 
 if($ok==1)
 {
@@ -41,15 +32,12 @@ $ok=0;
 if($regno=='0')
 {
 $sl="UPDATE totalmarks SET count=count+3 WHERE uuid='$uuid' ";
-echo 'Hello UUid';
 }
 else
 {
 $sl="UPDATE totalmarks SET count=count+3 WHERE emailid='$email' ";	
-echo 'Hello Email';
 }
 if ($conn->query($sl) === TRUE) {
-
 echo "sucessfully";
 
 }
@@ -59,6 +47,7 @@ else {
 }
 
 }
+
 	$conn->close();
 	exit;
 ?>	
