1758308d4467007437a2d3d829965e9826f4039d
zeeshan <zee2790@gmail.com>
2016-01-09 14:56:54 +0400

    bilal post

--- sendbilalpost.php
+++ sendbilalpost.php
@@ -2,34 +2,29 @@
 $servername = "54.169.152.134";
 $username = "root";
 $password = "hassan";
+$dbname = "a3214356_yikyak";
 
-//$dbname = "a3214356_yikyak";
-$dbname = "Bilal";
+
 $conn = new mysqli($servername, $username, $password, $dbname);
 // Check connection
 if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 } 
-$Id = $_POST["Id"];
-$Name = $_POST["Name"];
-$Id='10o000';
-$Name='asd';
-$sql = "INSERT INTO person (ID,Name)
+//$Id = $_POST["Id"];
+//$Name = $_POST["Name"];
 
-VALUES ('$Id','$Name');
+$Id='111';
+$Name='kkk';
+$sql = "INSERT INTO person (Id,Name)
+
+VALUES ('$Id','$Name')";
 
 if ($conn->query($sql) === TRUE) {
-    
+    echo "New record created successfully";
 
-} 
-
-else {
-
+} else {
+    echo "Error: " . $sql . "<br>" . $conn->error;
 }
 
-
-
 $conn->close();
-
-
-?>		
+?>
