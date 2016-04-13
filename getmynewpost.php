651e50bb1ecc2406876088aef7e5af6018e0e598
zeeshan <zee2790@gmail.com>
2016-04-13 11:51:29 +0400

    hello

--- getmynewpost.php
+++ getmynewpost.php
@@ -17,17 +17,22 @@ $mydate= date("Y-m-d H:i:s");
   // $id=(int)$id;
 
 	//$sql = "select * from mynewpost where id>'$id' order by id " ;
-$sql ="SELECT * FROM mynewpost ";
+$sql ="SELECT * FROM mynewpost  order by id";
 	//$sql = "select * from mynewpost where id>'1' order by id";
+
 	$result = $conn->query($sql);
+	echo "hello";
 	$userData = array();
 	if ($result->num_rows > 0) {
 	    // output data of each row
+	    	//echo "world";
 	    while($row = $result->fetch_assoc()) {
 			$tmp = array('id' => $row["id"],'uuid' => $row["uuid"],'email' => $row["email"], 'posttext' => $row["posttext"], 'imageurl' => $row["imageurl"],'count' => $row["count"],'gcmid' => $row["gcmid"],'orientation' => $row["orientation"]
 
                                                     , 'myuni' => $row["myuni"], 'olddate' => $row["olddate"], 'cdate' => $mydate);
 			array_push($userData, $tmp);
+			//echo $row["id"];
+			//echo $row["email"];
 			// 	$tmp = array('id' => $row["id"],'uuid' => $row["uuid"],'email' => $row["email"], 'posttext' => $row["posttext"], 'imageurl' => $row["imageurl"],'count' => $row["count"],'gcmid' => $row["gcmid"],'orientation' => $row["orientation"]
 
    //                                                  , 'myuni' => "OOOOO");
@@ -35,8 +40,15 @@ $sql ="SELECT * FROM mynewpost ";
 		
 		}
 	}
+	//else
+	//{
+			//echo "NOT";
+	//}
 //$userData = array_reverse($userData,true);
-	echo json_encode($userData);
+//echo "\n";
+//echo count($userData);
+//	echo json_encode($userData);
+echo json_encode($userData);
 	$conn->close();
 	exit;
 ?>	
